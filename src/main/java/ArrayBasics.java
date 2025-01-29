b8a4474702333d8b4d00d7f35a7fab098808c93c
GregorStallmeister <post@gregor-stallmeister.de>
2025-01-29 11:24:25 +0100

    add function findlargestNumber

--- src/main/java/ArrayBasics.java
+++ src/main/java/ArrayBasics.java
@@ -1,5 +1,19 @@
 public class ArrayBasics {
     public static void main (String[] args) {
-        
+        int[] numbers = {1, 2, 3, 4, 10, 6, 7, 8, 9, 5};
+
+        System.out.println(findLargestNumber(numbers));
+    }
+
+    public static int findLargestNumber (int[] numbers) {
+        int largest = 0;
+
+        for(int number : numbers) {
+            if (number > largest) {
+                largest = number;
+            }
+        }
+
+        return largest;
     }
 }
